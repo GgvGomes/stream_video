52a58a2e7ef105d98d15a87f73b82b400803b72e
Gabriel Gomes <ggvgabriel05@gmail.com>
2023-10-24 16:48:32 -0300

    comunicao com o rocketchat feita

--- src/App.tsx
+++ src/App.tsx
@@ -117,11 +117,12 @@ function App() {
       "Content-Type": "application/json",
       Accept: "*/*",
     },
+    baseURL: import.meta.env.VITE_DB_URL_LOCAL + "Video",
   });
 
   const postVideo = () => {
     api
-      .post(import.meta.env.VITE_DB_URL_LOCAL + "Video/UploadVideo", {
+      .post("/UploadVideo", {
         file: video64,
       })
       .then((response) => {
@@ -130,8 +131,10 @@ function App() {
   };
 
   const sendToRocketChat = () => {
-
-  }
+    api.post(`/SendVideotoRocketChat`, { file: video64 }).then((response) => {
+      console.log(response);
+    }); 
+  };
 
   return (
     <div className="w-full h-full flex flex-wrap justify-center align-top pt-20">
@@ -151,12 +154,13 @@ function App() {
         </div>
 
         <div className="flex w-[67%] justify-end gap-x-6">
-          <button
+          {/* <button
+            onClick={resetStates}
             disabled={true}
             className="mr-auto text-muted-foreground/80 px-6 h-12 text-lg inline-flex items-center justify-center rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border-2 shadow 
           hover:bg-red-500/80 hover:text-zinc-300">
             <Trash size={20} className="mr-2" /> Descartar
-          </button>
+          </button> */}
 
           {!permission && (
             <button
@@ -204,9 +208,9 @@ function App() {
           </button>
           <button
             onClick={sendToRocketChat}
-            disabled={recordedVideo === null && recordingStatus === "inactive"}
-            className="px-6 h-12 text-lg inline-flex items-center justify-center rounded-md  font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-green-500 text-primary-foreground shadow hover:bg-primary/90">
-            <Rocket size={20} className="mr-2" /> Enviar 
+            // disabled={recordedVideo === null && recordingStatus === "inactive"}
+            className="px-6 h-12 text-lg inline-flex items-center justify-center rounded-md  font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-green-500 text-primary-foreground shadow hover:bg-green-500/90">
+            <Rocket size={20} className="mr-2" /> Enviar
           </button>
         </div>
       </div>
